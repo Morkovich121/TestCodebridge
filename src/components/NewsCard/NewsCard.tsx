8ae35806946a5e032a661e048333bb4105cc0929
Ilya Sereda <ilyasereda121@gmail.com>
2023-01-19 00:21:28 +0200

    feat: done ArticlePage with correct articles on it

--- src/components/NewsCard/NewsCard.tsx
+++ src/components/NewsCard/NewsCard.tsx
@@ -25,36 +25,41 @@ const NewsCard: React.FC<Article> = ({ id, featured, title, url, imageUrl, newsS
     const link = "article/" + id;
 
     return (
-        <Card
-            className="card"
-            sx={{
-                border: "1px solid #EAEAEA", boxShadow: "0px 8px 24px rgba(0, 0, 0, 0.05)",
-                borderRadius: "5px", width: 400, maxWidth: 400, minHeight: 480, height: "fit-content"
-            }}>
-            <CardMedia
-                sx={{ height: 217 }}
-                image={imageUrl}
-                title="newsImage"
-            />
-            <CardContent className="cardContent" sx={{ paddingLeft: "20px" }}>
-                <Typography className="cardContent__date" sx={{ fontSize: "14px", opacity: "60%", fontFamily: "Montserrat, sans-serif" }}>
-                    <img src={timetable} alt="" className='cardContent__date-timetable' /> {publishedAt}
-                </Typography>
-                <Typography sx={{ fontSize: "24px", fontFamily: "Montserrat, sans-serif" }}>
-                    {title}
-                </Typography>
-                <Typography sx={{ fontSize: "16px", fontFamily: "Montserrat, sans-serif" }}>
-                    {summary.length > 130 ? summary.substring(0, 130) : summary}...
-                </Typography>
-                <Link href={link} className='link' sx={{
-                    textDecoration: "none", color: "black", fontSize: "14px", fontWeight: "700",
-                    fontFamily: "Montserrat, sans-serif",
+        <Link href={link} sx={{
+            textDecoration: "none", color: "black", fontSize: "14px", fontWeight: "700",
+            fontFamily: "Montserrat, sans-serif",
+        }}>
+            <Card
+                className="card"
+                sx={{
+                    border: "1px solid #EAEAEA", boxShadow: "0px 8px 24px rgba(0, 0, 0, 0.05)",
+                    borderRadius: "5px", width: 400, maxWidth: 400, minHeight: 480, height: "fit-content"
                 }}>
-                    Read more <img src={rightArrow} alt="" className='linkArrow' />
-                </Link>
-            </CardContent>
+                <CardMedia
+                    sx={{ height: 217 }}
+                    image={imageUrl}
+                    title="newsImage"
+                />
+                <CardContent className="cardContent" sx={{ paddingLeft: "20px" }}>
+                    <Typography className="cardContent__date" sx={{ fontSize: "14px", opacity: "60%", fontFamily: "Montserrat, sans-serif" }}>
+                        <img src={timetable} alt="" className='cardContent__date-timetable' /> {publishedAt.substring(0, 10)}
+                    </Typography>
+                    <Typography sx={{ fontSize: "24px", fontFamily: "Montserrat, sans-serif" }}>
+                        {title}
+                    </Typography>
+                    <Typography sx={{ fontSize: "16px", fontFamily: "Montserrat, sans-serif" }}>
+                        {summary.length > 130 ? summary.substring(0, 130) : summary}...
+                    </Typography>
+                    <Link href={link} className='link' sx={{
+                        textDecoration: "none", color: "black", fontSize: "14px", fontWeight: "700",
+                        fontFamily: "Montserrat, sans-serif",
+                    }}>
+                        Read more <img src={rightArrow} alt="" className='linkArrow' />
+                    </Link>
+                </CardContent>
 
-        </Card>
+            </Card>
+        </Link>
     )
 }
 
